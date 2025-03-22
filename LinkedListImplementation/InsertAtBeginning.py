d5580584a0558fa15a0894cf4401c728f279d4f7
Hepsi2002 <159252986+Hepsi2002@users.noreply.github.com>
2025-03-22 16:10:38 +0530

    Insertion at beginning of the node.

--- LinkedListImplementation/InsertAtBeginning.py
+++ LinkedListImplementation/InsertAtBeginning.py
@@ -16,14 +16,16 @@ class LinkedList:
         return self._size
     def isempty(self):#this is our own defined method.Where as __init__ and __len__ are built-in class level functions.
         return self._size == 0 #True if size empty and false if size is not zero
+
     def addlast(self,e):#value of the element to be inserted.
-        newest = _Node(e,None)
+        newest = _Node(e,None)#Object is created
         if self.isempty():
-            self._head = newest
+            self._head = newest#This points to the first node(Newest node)
         else:
-            self._tail._next = newest
-        self._tail = newest
-        self._size += 1
+            self._tail._next = newest#Link is created
+        self._tail = newest#The newest element will have its tail none.
+        self._size += 1#Sizeis incremented as new node is added
+    #Time complexity is O(1) takes unit time to compute.
 
     def insert_begin(self,e):
         newest = _Node(e,None)#New node is created
@@ -36,21 +38,22 @@ class LinkedList:
         self._size +=1#size will be incremented as new node is added
     #Time complexity of the insert_begin function will be O(1)
     def search(self,key):
-        p = self._head
+        p = self._head#To keep track of node & the linked list
         index = 0
         while p:
             if p._element == key:
                 return index
-            p = p._next
+            p = p._next#if element is not equal to key, p value will be moved
             index=index+1
-
+        return -1#when key is not found
 
     def display(self):
         p=self._head
-        while p:
+        while p:#we can use for loop but we have used while loop as we need size.
             print(p._element,end='-->')
-            p = p._next
+            p = p._next#For the last node the value of p will be none so the loop will terminate
         print()
+    #Time complexity of the display method will be O(N).
 
 
 L = LinkedList()
