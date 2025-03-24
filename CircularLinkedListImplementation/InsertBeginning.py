d40477513f2e29607f4984adbac616adc2860424
Hepsi2002 <159252986+Hepsi2002@users.noreply.github.com>
2025-03-24 21:40:42 +0530

    Inserting at the beginning of the list.

---
+++ CircularLinkedListImplementation/InsertBeginning.py
@@ -0,0 +1,62 @@
+class _Node:
+    __slots__ = '_element','_next'
+    def __init__(self,element,next):
+        self._element = element
+        self._next = next
+
+class CircularLinkedList:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def isempty(self):
+        return self._size == 0
+
+    def addlast(self,e):
+        newest = _Node(e,None)
+        if self.isempty():
+            newest._next = newest
+            self._head = newest
+        else:
+            newest._next = self._tail._next
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+    def insert_begin(self,e):
+        newest = _Node(e,None)
+        if self.isempty():
+            newest._next = newest
+            self._head = newest
+        else:
+            self._tail._next = newest
+            newest._next = self._head
+        self._head = newest
+        self._size += 1
+
+    def display(self):
+        p = self._head
+        i=0
+        while i < len(self):
+            print(p._element,end = "-->")
+            p = p._next
+            i = i + 1
+        print()
+
+c = CircularLinkedList()
+c.addlast(2)
+c.addlast(6)
+c.addlast(20)
+c.addlast(7)
+c.addlast(5)
+c.addlast(70)
+c.display()
+print('Size',len(c))
+c.insert_begin(23)
+c.insert_begin(15)
+c.display()
+print('Size',len(c))
